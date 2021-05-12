5bce03e01165adc48f72dc39cbc336d136dc1421
marvzhang <tikazyq@163.com>
2021-05-12 22:06:25 +0800

    added services

---
+++ dist/js/streammessagecode.js
@@ -0,0 +1,21 @@
+// source: entity/stream_message_code.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.grpc.StreamMessageCode');
+
+/**
+ * @enum {number}
+ */
+proto.grpc.StreamMessageCode = {
+  PING: 0
+};
+
